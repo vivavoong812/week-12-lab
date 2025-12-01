6b32e6f3085e4fbee0093b3f50f84cb33c736f47
graceqli <grace_q_li@brown.edu>
2025-12-01 18:30:54 -0500

    Update simulation.R

--- 2 - Scripts/simulation.R
+++ 2 - Scripts/simulation.R
@@ -1,4 +1,10 @@
-
+#' Simulating one day of trips for a single pair of start and end stations
+#'
+#' @description This function simulates arrival times for a single pair of start an end stations using non-homogeneous Poisson. 
+#'
+#' @param arrival rates A data frame with the columns: start_station, end_station, hour, and mu_hat.
+#'
+#' @return a data frame with the columns: start_station, end_station, and time.
 
 sim_trip_one <- function(arrival_rates) {
 
@@ -16,6 +22,7 @@ sim_trip_one <- function(arrival_rates) {
   time <- 0
   arrivals <- c()
 
+  # simulate arrivals over 24 hours
   while (current_time < 24) {
     rate <- rexp(1,1 rate = lambda_max)
     time <- time + 1
@@ -29,6 +36,7 @@ sim_trip_one <- function(arrival_rates) {
       filter(hour == current_hour) %>%
       pull(mu_hat)
 
+    # if there is no rate, go to next iteration
     if (length(lambda) == 0 || is.na(lambda[1])) next
     lambda <- lambda[1]
 
@@ -37,13 +45,23 @@ sim_trip_one <- function(arrival_rates) {
         arrivals <- c(arrivals, time)
     }
   }
+  
   # in case of no arrivals
   if (length(arrivals == 0) return(NULL)
 
-  data.frame(start_station = arrival_rates$start_station[1],
-             end_station = end_rates$start_station[1],
-             time = arrivals)
+  # results in data frame
+  arrival_results <- data.frame(start_station = arrival_rates$start_station[1],
+                     end_station = end_rates$start_station[1],
+                     time = arrivals)
 }
+
+#' Simulating one day of trips for all station trips
+#'
+#' @description This function takes the sim_trip_one function and applies it to all pairs of start and end station
+#'
+#' @param arrival_rate A data frame with the columns: start_station, end_station, hour, and mu_hat
+#'
+#' @return A data frame with columns: start_station, end_station, and time (arrival_times)
 
 sim_trips_all <- function(arrival_rates) {
   set.seed(123)
