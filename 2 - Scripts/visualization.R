d4e17be977551f3745058ff6d042237605083141
graceqli <grace_q_li@brown.edu>
2025-12-01 21:16:28 -0500

    Update visualization.R

--- 2 - Scripts/visualization.R
+++ 2 - Scripts/visualization.R
@@ -1,3 +1,11 @@
+#' Visualizing the expected bike demand and bike allocation
+#'
+#' @description A function that creates a scatterplot of the expected bike demand and the final bike allocation
+#'
+#' @param arrival_rates A data frame with the columns: start_station, end_station, hour, and mu_hat
+#'        placement_results A data frme with the columns: start_station and num_bikes
+#'
+#' @return A scatterplot with expected demand on the x-axis and number of bike allocated on the y-axis
 
 visualization <- function(arrival_rates, placement_results) {
   
@@ -17,7 +25,6 @@ visualization <- function(arrival_rates, placement_results) {
          y = "Number of Bikes",
          title = "Demand vs. Bike Allocation") +
     theme_minimal()
-    
 }
 
 visualize_results <- visualization(arrival_rates, placement_results)
