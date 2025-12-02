87f8038555f03bff59377022a9de5fc5b0d244c1
vivavoong812 <vivavoong@gmail.com>
2025-12-01 22:39:45 -0500

    Update simulation_test.R

--- 3 - Tests/simulation_test.R
+++ 3 - Tests/simulation_test.R
@@ -1,8 +1,8 @@
 arrival_rates <- data.frame(
-    start_station = c(1, 1, 2),
-    end_station   = c(2, 3, 3),
-    hour          = c(8, 9, 10),
-    mu_hat        = c(0.5, 1.0, 0.3)
+    start_station = c(1, 1, 2, 3),
+    end_station   = c(2, 3, 3, 5),
+    hour          = c(8, 9, 10, 11),
+    mu_hat        = c(0.5, 1.0, 0.3, 0.4)
   )
   
   simulate_demand(arrival_rates)
